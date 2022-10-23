e89ac94bb5ab3964f8e4b7b7e6840c642234d732
Pozsár Zsolt <pozsarzs@karak>
2022-10-23 21:26:07 +0200

    221023

--- source/mini_serial_console.ino
+++ source/mini_serial_console.ino
@@ -5,12 +5,49 @@
 // | Program for Raspberry Pi Pico                                             |
 // +---------------------------------------------------------------------------+
 
-//   This program is free software: you can redistribute it and/or modify it
-// under the terms of the European Union Public License 1.2 version.
-//
-//   This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.
+/*
+      This program is free software: you can redistribute it and/or modify it
+    under the terms of the European Union Public License 1.2 version.
+
+      This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.
+*/
+
+/* Operation modes
+
+   Mode #0: read only mode,
+            no cursor,
+            20x4 size displayed area on 80x4 size virtual screen,
+            automatically scrolling lines,
+            the displayed area can be moved horizontally with pushbuttons.
+   Mode #1: read only mode,
+            no cursor,
+            20x4 size displayed area on 80x25 size virtual screen,
+            automatically scrolling lines,
+            the displayed area can be moved horizontally and vertically with pushbuttons.
+   Mode #2: read only mode,
+            no cursor,
+            20x4 size displayed area on 80x25 size virtual screen,
+            the displayed area can be moved horizontally and vertically with pushbuttons,
+            after 2000 characters or FormFeed (12H), a new, clean page starts.
+   Mode #3: reserved to read/write mode for any devices
+
+
+   Button functions
+
+        |  Mode  #0  |  Mode  #1  |  Mode  #2  |  Mode #3
+   -----+------------++-----------+-----------+-----------
+    PB0 | move left  | move left  | move left  | move left
+    PB1 | move right | move right | move right | move right
+    PB2 |            | move up    | move up    | move up
+    PB3 |            | move down  | move down  | move down
+    PB4 |            |            |            | enter
+    PB5 |            |            |            | escape
+
+*/
+
+# define TEST
 
 // settings
 const int     spd_serial0       = 115200;  // speed of the USB serial port
@@ -42,7 +79,7 @@ const int     prt_led           = LED_BUILTIN;
 // general constants
 const String  swversion         = "0.1";   // version of this program
 // general variables
-int           opmode            = 0;       // operation mode
+int puffer[80][25];                        // virtual display
 // messages
 String msg[40]                  =
 {
@@ -51,12 +88,12 @@ String msg[40]                  =
   /*  2 */  "Software: v",
   /*  3 */  "(C)2022 Pozsar Zsolt",
   /*  4 */  "Initializing...",
-  /*  5 */  " * Set speed of other serial ports: ",
+  /*  5 */  " * Set speed of serial ports #1-#2: ",
   /*  6 */  " * Set GPIO ports",
-  /*  7 */  "",
-  /*  8 */  "",
-  /*  9 */  "",
-  /* 10 */  "",
+  /*  7 */  " * Set operation mode of LCD",
+  /*  8 */  " * Operation mode of mini console device is #",
+  /*  9 */  " * WARNING! Mini console is in the test mode.",
+  /* 10 */  "Read data from serial port #",
   /* 11 */  "",
   /* 12 */  "",
   /* 13 */  "",
@@ -127,24 +164,107 @@ void setup() {
   pinMode(prt_pb3, INPUT);
   pinMode(prt_pb4, INPUT);
   pinMode(prt_pb5, INPUT);
+  // Set LCD
+  Serial.println(msg[7]);
+  lcd_init();
+#ifdef TEST
+  Serial.println(msg[9]);
+  for (int r = 1; r < 26; r++) {
+    for (int c = 1; c < 81; c++) {
+      if (c == 1) {
+        if (r < 10) {
+          puffer[c - 1][r - 1] = 48;
+        }
+        if (r > 9 and r < 20) {
+          puffer[c - 1][r - 1] = 49;
+        }
+        if (r > 19) {
+          puffer[c - 1][r - 1] = 50;
+        }
+      }
+      if (c == 2) {
+        if (r < 10) {
+          puffer[c - 1][r - 1] = int(r);
+        }
+        if (r > 9 and c < 20) {
+          puffer[c - 1][r - 1] = int(r - 10);
+        }
+        if (r > 19) {
+          puffer[c - 1][r - 1] = int(r - 20);
+        }
+      }
+      if (c == 3) {
+        puffer[c - 1][r - 1] = 95;
+      }
+      if (c >= 4) {
+        if (c >= 4 and c <= 29) {
+          puffer[c - 1][r - 1] = c + 93;
+        }
+        if (c == 30 or c == 57) {
+          puffer[c - 1][r - 1] = 32;
+        }
+        if (c >= 31 and c <= 56) {
+          puffer[c - 1][r - 1] = c + 34;
+        }
+        if (c >= 58) {
+          puffer[c - 1][r - 1] = c + 39;
+        }
+      }
+    }
+  }
+#endif
 }
 
 // main function
 void loop() {
-
+  int mode = 0;                            // operation mode
+  // get operation mode
+  mode = digitalRead(prt_jp3) * 2 + digitalRead(prt_jp3);
+  Serial.println(msg[8] + String((int)mode));
+#ifndef TEST
+  // handling serial ports
+  com_handler(1);
+  com_handler(2);
+#endif
+  // handling buttons
+  btn_handler(mode);
 }
 
-// sign serial port activity
-void led(int i) {
-  if (i == 0) {
-    digitalWrite(prt_led, LOW);
-  } else {
-    digitalWrite(prt_led, HIGH);
+// read communication port and move data to puffer
+void com_handler(int p) {
+  switch (p) {
+    case 1:
+      if (Serial1.available()) {
+        Serial.println(msg[10] + '1');
+        digitalWrite(prt_led, HIGH);
+        //        Serial1.read();
+      }
+      break;
+    case 2:
+      if (Serial2.available()) {
+        Serial.println(msg[10] + '2');
+        digitalWrite(prt_led, HIGH);
+        //        Serial2.read();
+      }
+      break;
+    default:
+      digitalWrite(prt_led, LOW);
+      break;
   }
 }
 
+// read status of pushbuttons and write text to LCD
+void btn_handler(int m) {
+
+}
+
+// initialize LCD
+void lcd_init() {
+
+}
+
 // turn on/off background light
-void display_backgroundlight(int i) {
+void lcd_backgroundlight(int i) {
   if (i == 0) {
     digitalWrite(lcd_bl, LOW);
   } else {
@@ -153,6 +273,6 @@ void display_backgroundlight(int i) {
 }
 
 // write text to display
-void display_writetotext() {
+void lcd_writelines(int x1, int y1, int x2, int y2) {
 
 }
